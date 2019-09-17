bb5dfea35833aa200dfdb14f6a782506b8a6cb53
CalebMonestime <54561004+CalebMonestime@users.noreply.github.com>
2019-09-16 21:37:29 -0400

    Add files via upload

--- Workbook 2.1.py
+++ Workbook 2.1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import pandas as pd
@@ -9,11 +9,17 @@ import numpy as np
 import glob
 
 all_data = pd.DataFrame()
-for f in glob.glob("datasets/weekly_call_data/weekdata*.xlsx"):
+for f in glob.glob("datasets/datasets/weekly_call_data/weekdata*.xlsx"):
     print f
     df = pd.read_excel(f)
     all_data = all_data.append(df,ignore_index=True)
-    all_data.describe()
+all_data.describe()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
