fa0d503ad2e492f138459f2e47a146c22e657368
CalebMonestime <54561004+CalebMonestime@users.noreply.github.com>
2019-10-20 14:50:44 -0400

    Add files via upload

---
+++ Workbook Assignment 7.1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+names = ['Bob','Jessica','Mary','John','Mel']
+status = ['Senior','Freshman', 'Sophmore','Senior','Junior']
+grades = [76,95,77,78,99]
+GradeList = zip(names,grades)
+df = pd.DataFrame(data = GradeList, columns=['Names', 'Grades'])
+
+get_ipython().magic(u'matplotlib inline')
+df.plot(kind='bar')
+
+
+# In[4]:
+
+
+df2 = df.set_index(df['Names'])
+df2.plot(kind="bar")
+
+
+# In[ ]:
+
+
+
+
